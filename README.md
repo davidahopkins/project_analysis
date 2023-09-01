448175a990a13cb402c763bcabe7276f213438e7
davidahopkins <113660848+davidahopkins@users.noreply.github.com>
2023-09-01 07:40:29 -0700

    updates, new stuff
    updates readme, added new folder with document log creation script

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-# cost-schedule_estimating-analysis
-This is a public repository for cost &amp; schedule estimating and analysis professionals to share code ideas
+# Cost & Schedule Analysis
+This is a public repository for Cost Esimating &amp; Schedulding professionals to share code ideas
 
 ## Example Folder
 The example folder contains, well, an example, a small piece of code that can take a string of feet & inches and convert it to a float value.
 As new project get introduced each can have its own folder containing all the elements of that project. 
 Please feel free to create folders as needed for different ideas.
+
+## Document Log
+The document log folder contains two file, the python file and a jupyter notebook. The script will look through a directory and extract the names and file paths of all files in the directory, then create an excel workbook with the name of the files as hyperlinks to their locations. 
+
+When the script is run it will ask where your directory is, where you want to save the new workbook, and what your project number is. 
+
+The excel workbook will have the following columns:
+- index: from the dataframe
+- Date: user define once the workbook is made
+- Document Type: user defined once the workbook is made
+- Name: this is the file name from the directory
+- Path: this is the full path of the files from the directory
+- Name Match: this is a formula that takes the file name from the path and is used for the error checking
+- Error Checking: this is a formula that checks is the file name from the path and the file name match, if the value is true the files and paths are in correct order, if it is false there is an error.
+- Document: this is a formala that lists the file names and creates a hyperlink to the file path, these will not show up blue underlined like a typical link, but click on one of the cells and it should open the file from that path
---
+++ document_log/doc_log.py
@@ -0,0 +1,65 @@
+#import required packages
+
+import numpy as np
+import pandas as pd
+import os
+import time
+import glob
+from openpyxl import Workbook
+
+#create user inputs for script
+dir_path = input('what is the file path?')
+
+save_to_current = input('do you want to save output to another location? [y/n]?')
+if save_to_current == 'y':
+    save_path = dir_path
+else:
+    save_path = input('what is the file path to save to?')
+
+project_number = str(input('what is the project #?'))
+
+#start the timer
+start = time.perf_counter()
+
+#create a blank list for file names
+file_name = []
+
+#loop through directory and list all file names
+for (root, dirs, file) in os.walk(dir_path):
+    for f in file:
+        file_name.append(f)
+
+#loop through the directory and list all file paths
+file_path = [os.path.join(dirpath, f) for (dirpath, dirnames, filenames) in os.walk(dir_path) for f in filenames]
+
+#create list length
+count = len(file_name)
+
+#create dictionary from the list of file names and paths
+file_dict = {file_name[i]: file_path[i] for i in range(len(file_name))}
+
+#create dataframe from dictionary
+df = pd.DataFrame(list(zip(file_name, file_path)), columns=['Name', 'Path'])
+
+#create columns for dataframe
+df.insert(0,'Date','')
+df.insert(1,'Document Type','')
+df['Name Match'] = ''
+df['Error Checking'] = ''
+df['Document'] = ''
+df['Author'] = ''
+df['Amount'] = ''
+
+#loop rhrough data and create hyperlinks and error checking of dataframe
+for i in range(count):
+    j =+ i+1
+    df.loc[i,'Document'] = '=HYPERLINK(E{}, D{})'.format(j+1,j+1)
+    df.loc[i,'Name Match'] = '=TRIM(RIGHT(SUBSTITUTE(E{},"\\",REPT(" ",255)),255))'.format(j+1,j+1)
+    df.loc[i, 'Error Checking'] = '=D{}=F{}'.format(j+1,j+1)
+
+#export dataframe to excel
+df.to_excel(save_path+'\\'+project_number+'.Document Log.xlsx')
+
+#stop the timer
+stop = time.perf_counter()
+print(f'A total of {count} documents were listed in {stop - start:0.2f} seconds')
